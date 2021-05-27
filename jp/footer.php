4484d749e3fdb592b0c188a64777fa2f844cffe6
katchion13 <69910600+katchion13@users.noreply.github.com>
2021-05-27 22:32:23 +0900

    Add files via upload

---
+++ jp/footer.php
@@ -0,0 +1,46 @@
+
+	<div id="footerwrap"><div class="container" id="contact" name="contact"><div class="row"  style="color:white;"><div class="col-lg-4">
+					<h4 style="color:white;">コンタクト先</h4><br/>
+					<p>🏘&nbsp 東京都世田谷区<br/>☏	+81 ☆☆☆☆ 6464 <br/>
+	✉&nbsp katchion@gmail.com<br/>
+							</p>
+				</div><!-- col -->
+				
+				<div class="col-lg-4">
+					<h4 style="color:white;">SNS</h4><br/>
+					<a href="https://www.facebook.com/katchion"><span style="color:white;">ⓕ&nbsp Katia Midori</span></a> <br/>
+					<sp<a href="https://www.instagram.com/katchion"><span style="color:white;">👩&nbsp @katchion </span></a> <br/>
+		💬  ID:katchion</p>
+				</div><!-- col -->
+				
+				<div class="col-lg-4"><h4 style="color:white;">お問い合わせ・質問</h4><br/>
+					<p>お問い合わせ、質問やオファーなどあればメールでコンタクトください。</p>
+					</div><!-- col -->	</div><!-- row -->		
+						<h4>Created by katchion - Copyright 2019</h4></div>	</div>
+		
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+		
+ 
+	<script type="text/javascript" src="http://katchion.com/assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="http://katchion.com/assets/js/retina.js"></script>
+	<script type="text/javascript" src="http://katchion.com/assets/js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="http://katchion.com/assets/js/smoothscroll.js"></script>
+	<script type="text/javascript" src="http://katchion.com/assets/js/jquery-func.js"></script>
+	
+	
+	
+					
+					
+				</div>
+
+			</div><!-- .boxed-wrapper -->
+
+		</footer><!-- #page-footer -->
+
+	</div><!-- #page-wrap -->
+
+</body>
+</html>
---
+++ jp/main.php
@@ -0,0 +1,247 @@
+<?php include 'header.php';?>
+
+
+		<div class="row white centered">
+			   				<h2>記事</h2>
+				<hr>
+			</div>
+	<div class="container">
+			<div class="row white"><div class="col-lg-6">
+				<br>
+<b>HTML・練習の仕方</b><br>
+<br>
+HTMLは有名なので、その説明はしませんがCSSとHTMLを誰もが簡単に練習出来るように説明します。	<br>
+	<br>
+それは、なんと… 皆が知っているメモ帳！！	<br>
+	<br>
+色んな所でHTMLは使えますが、いざコードがなければ時間が掛かると思うので…	<br>
+暇な時パソコンにいたらメモ帳を開いてググったコードをコピペして保存しておきましょう。	<br>
+	<br>
+<br>
+<a href="http://katchion.com/2020/4/1.html">⇒続きはこちら <br> 
+</a><br>
+</div>
+	<div class="col-lg-6"><br>
+<b>CSS・練習の仕方</b><br>
+<br>
+HTMLは有名なので、その説明はしませんがCSSとHTMLを誰もが簡単に練習出来るように説明します。	<br>
+	<br>
+	CSSも探せばいくらでもネット見つかるので、今回もメモ君で簡単に練習する方法を説明します。	<br>
+	<br>
+前の記事で説明した　index.htmlをそのまま使い続けます。	<br>
+HTMLベーシックである頭や体はまだ書く必要がありません、まずは使い方を説明するので。	<br>
+	<br>
+<br>
+<a href="http://katchion.com/2020/5/1.html">⇒続きはこちら <br> 
+</a><br>
+
+ 
+<br>   
+  </div></div>モチベアップにワンクリックプリーズ↓</div></div>
+ <BR><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-8709903709475115"
+     data-ad-slot="6911019671"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script> 
+	<div class="row white centered"><BR><BR>
+			   				<h2>ブログ</h2>
+				<hr>
+			</div>
+	
+		<div class="container" id="etc" name="etc">
+			<div class="row white centered">
+						<div class="col-lg-3 centered">
+				<a href="https://ameblo.jp/kuroki-chan/theme-10074138618.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20170508/17/kuroki-chan/af/30/p/o0400040013932624579.png" height="120px" width="120px" alt="">
+					<br><h4><b>哲学</b></h4></a>	</div><!-- col-lg-3 -->
+				<div class="col-lg-3 centered">
+				<a href="https://ameblo.jp/kuroki-chan/theme-10074543187.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20161022/14/kuroki-chan/41/08/j/o1536204813779073985.jpg" height="120px" width="120px" alt="">
+					<br>
+					<h4><b>スピリチュアル系一覧</b></h4></a><br/>
+					
+				</div><!-- col-lg-3 -->
+				
+				<div class="col-lg-3 centered">
+				<a href="https://ameblo.jp/kuroki-chan/theme-10100495029.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20170104/13/kuroki-chan/6e/cb/j/o1417165413837603010.jpg" height="120px" width="120px" alt="">
+					<br>
+					<h4><b>目標・結果</b></h4></a><br/></div><div class="col-lg-3 centered">
+				<a href="https://ameblo.jp/kuroki-chan/theme-10074543015.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20131125/09/kuroki-chan/20/c3/j/o0800106712760353237.jpg" height="120px" width="120px" alt="">
+					<br>
+					<h4><b>IT記事一覧</b></h4></a>	<br/>			
+				</div><!-- col-lg-3 --></div><!-- row --></div>
+	<div class="container" id="etc" name="etc"><div class="row white centered"><div class="col-lg-3 centered"><a href="https://ameblo.jp/kuroki-chan/theme-10091518743.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20150815/01/kuroki-chan/0a/3f/p/o0800060013396164806.png" height="120px" width="120px" alt="">	<br><h4><b>音楽一覧</b></h4></a>	</div><!-- col-lg-3 -->
+								<div class="col-lg-3 centered">	<a href="https://ameblo.jp/kuroki-chan/theme-10082998407.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20150901/13/kuroki-chan/7c/6e/j/o0417030313412687458.jpg" height="120px" width="120px" alt="">
+					<br>
+					<h4><b>映画一覧</b></h4></a><br/>
+					
+				</div><!-- col-lg-3 -->
+				
+				<div class="col-lg-3 centered">
+				<a href="https://ameblo.jp/kuroki-chan/theme-10074542733.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20131011/21/kuroki-chan/80/22/j/o0259019412712824011.jpg" height="120px" width="120px" alt="">
+					<br>
+					<h4><b>ブラジル</b></h4></a><br/>
+					
+				</div><!-- col-lg-3 -->
+				
+				<div class="col-lg-3 centered">
+				<a href="https://ameblo.jp/kuroki-chan/theme-10030705572.html" target="_blank" ><img class="img img-circle" src="https://stat.ameba.jp/user_images/20131125/09/kuroki-chan/f2/30/j/o0800106712760336515.jpg" height="120px" width="120px" alt="">
+					<br>
+					<h4><b>その他</b></h4></a><br/>
+					
+				</div><!-- col-lg-3 --></div><!-- row --></div><!-- container -->
+		
+		
+		
+		<section class="section-divider textdivider divider2">
+			<div class="container"  id="portfolio" name="portfolio">
+				<h1 class="a">ポートフォリオ</h1>
+				<hr>
+			<h3 class="a">軽く今までやった事の紹介</h3>
+			</div><!-- container -->
+		</section><!-- section -->
+
+		<!-- ==== PORTFOLIO ==== -->
+		<div class="container" >
+		<br>
+		
+		<br>		
+			<div class="container">
+			<div class="row">	
+		
+				<div class="col-md-4 ">
+			    	<div class="grid mask">
+						<figure>
+					<a data-toggle="modal" href="http://katchion.com/bootstrap/" target="_blank"  class="btn btn-primary btn-lg">		<img class="img-responsive" src="assets/img/portfolio/port01.jpg" alt=""></a>
+							<figcaption>
+								<h5>レスポンシブ・テーブル</h5>
+								<a data-toggle="modal" href="http://katchion.com/bootstrap/" target="_blank"  class="btn btn-primary btn-lg">クリック</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+				<!-- PORTFOLIO IMAGE 2 -->
+				<!-- PORTFOLIO IMAGE 5 -->
+				<div class="col-md-4">
+			    	<div class="grid mask">
+						<figure>
+							<a data-toggle="modal" href="http://jp.enkyo.org.br/" target="_blank" class="btn btn-primary btn-lg"><img class="img-responsive" src="assets/img/portfolio/port05.jpg" alt=""></a>
+							<figcaption>
+								<h5>Enkyo</h5>
+								<a data-toggle="modal" href="http://jp.enkyo.org.br/" target="_blank" class="btn btn-primary btn-lg">クリック</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+				
+				<!-- PORTFOLIO IMAGE 6 -->
+				<div class="col-md-4">
+			    	<div class="grid mask">
+						<figure>
+						<a data-toggle="modal" href="https://his-brasil.com.br/" target="_blank" class="btn btn-primary btn-lg">	<img class="img-responsive" src="assets/img/portfolio/port06.jpg" alt=""></a>
+							<figcaption>
+								<h5>H.I.S.</h5>
+								<a data-toggle="modal" href="https://his-brasil.com.br/" target="_blank" class="btn btn-primary btn-lg">クリック</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+			</div><!-- /row -->
+							</div><!-- /row -->
+	</div><!-- /container -->
+	
+		<div class="container" >
+		<div class="container">
+			<div class="row">	
+		
+				<div class="col-md-4 ">
+			    	<div class="grid mask">
+						<figure>
+					<a data-toggle="modal" href="https://katchion.com/loja" target="_blank"  class="btn btn-primary btn-lg">		<img class="img-responsive" src="assets/img/portfolio/port04.jpg" alt=""></a>
+							<figcaption>
+								<h5>E-commerce</h5>
+								<a data-toggle="modal" href="https://katchion.com/loja" target="_blank"  class="btn btn-primary btn-lg">クリック</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+				<!-- PORTFOLIO IMAGE 2 -->
+				<!-- PORTFOLIO IMAGE 5 -->
+				<div class="col-md-4">
+			    	<div class="grid mask">
+						<figure>
+							<a data-toggle="modal" href="https://katchion.com/web/index.html" target="_blank" class="btn btn-primary btn-lg"><img class="img-responsive" src="assets/img/portfolio//port111.jpg" alt=""></a>
+							<figcaption>
+								<h5>UI・Design</h5>
+								<a data-toggle="modal" href="https://katchion.com/web/index.html" target="_blank" class="btn btn-primary btn-lg">クリック</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+				
+				<!-- PORTFOLIO IMAGE 6 -->
+				<div class="col-md-4">
+			    	<div class="grid mask">
+						<figure>
+						<a data-toggle="modal" href="https://katchion.com/wordpress" target="_blank" class="btn btn-primary btn-lg">	<img class="img-responsive" src="assets/img/portfolio/port022.jpg" alt=""></a>
+							<figcaption>
+								<h5>ワードプレス</h5>
+								<a data-toggle="modal" href="https://katchion.com/wordpress" target="_blank" class="btn btn-primary btn-lg">クリック</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+			</div><!-- /row -->
+							</div><!-- /row -->
+	</div><!-- /container -->
+	
+		<br><!-- ==== ABOUT ==== -->
+	
+	<center><div class="row">
+				<div class="col-lg-12 blog-bg">
+					<div class="col-lg-4 centered"><br><br>
+						<a href="https://katiayanaguisawa.portfoliobox.net/" target="_blank"><img class="img img-circle" src="assets/img/team/port10.jpg " width="85px" height="85px">
+						<h4>ポートフォリオ</h4></a>	
+					</div>	
+					<div class="col-lg-8 blog-content">
+			<h2>		ポートフォリオ</h2>
+			仕事用のポートフォリオです。サイトは個人の勉強サイトなのでついでに使ってるだけで、プロっぽくない
+								<p><a href="https://katiayanaguisawa.portfoliobox.net/" target="_blank">サイトを見る</a></p><br>
+						
+					</div>
+			</div>	</div></center><!-- /col -->
+			<br>	
+	
+<div class="cover1">
+<div class="container" id="about" name="about"><section class="section-divider textdivider"><div class="container"><h1 class="a">自己紹介</h1>
+				<hr >
+			<h3 class="a">最後に少しだけ自分の事を書きます</h3>
+			</div><!-- container -->
+		</section>
+			</div></div>
+    <center>
+    			<div class="container">
+			<div class="row white"><div class="col-lg-6">
+					<p>カチアはこのサイトを作っている日系ブラジル人4世｡　小学校の頃に親が日本へ働きに来て小学校5年生の終わりにブラジルへ帰る。その頃アニメや漫画に嵌る。高校を卒業して日本へ出稼ぎにくるもののこれではダメだと感じ母国へ勉強しに戻る。専門学校でプログラミングを勉強して、データベースの大学へ行きOracleDBAのMBAを中退。</p>
+			<br>
+				<a href="http://katchion.com/skill.html" target="_blank" ><img class="img img-circle" src="https://img.etimg.com/thumb/msid-61078146,width-300,imgsize-1050304,resizemode-4/unemployment-at-17-8-million-india-needs-to-look-at-skills-development-through-multiple-lenses.jpg" height="120px" width="120px" alt="">
+					<br>
+					<h4><b>Skill</b></h4></a>
+					
+					</div><!-- col-lg-6 -->
+				
+			<div class="col-lg-6">
+					<p>ウェブデザイナーとして働いたがまた日本へ出稼ぎに来る。初めはウェブデザイナー、フロントエンジニアから最終的にはデータベースエンジニアになりたいとは思っているがそれまでは先が長い。2019年の10月には東京へ来るが、決まりそうだった仕事が大阪と言われ半年いろなバイトをしてやっと落ち着いてきた？</p>
+	
+				<a href="http://katchion.com/hobby.html" target="_blank" ><img class="img img-circle" src="https://i0.wp.com/brentsully.com/wp-content/uploads/2019/02/positive-wxyz003-512.png?w=466&ssl=1" height="120px" width="120px" alt=""></a>
+					<br><!-- row -->
+					<h4><b>趣味</b></h4></div>
+				
+				
+		</div></div><br>
+
+
+<?php include 'footer.php';?>
